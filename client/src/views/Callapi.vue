9f748088382d25d7fac6c4a055357ae29833418a
tanakorn <tanakorn.peeti@gmail.com>
2020-03-06 14:19:24 +0700

    add service and add route

--- client/src/views/Callapi.vue
+++ client/src/views/Callapi.vue
@@ -3,6 +3,20 @@
         <h1>Call api</h1>
         <div class="container">
             <h1>Response from server</h1>
+
+            <button @click="fetchDatas">click to change data default</button>
+            <br />
+
+            <span>
+                <input
+                    placeholder="create data"
+                    type="text"
+                    v-model="info"
+                    @keyup.enter.prevent="todoClick"
+                />
+                <button @click="createData">click to add data</button> <br />
+            </span>
+
             {{ this.response }}
         </div>
     </div>
@@ -15,12 +29,25 @@ const apiUrl = 'http://localhost:3000';
 export default {
     data() {
         return {
-            response: ''
+            info: '',
+            response: []
         };
     },
     methods: {
         async fetchData() {
             const { data } = await Axios.get(apiUrl);
+            this.response.push(data);
+        },
+        async fetchDatas() {
+            const { data } = await Axios.get(apiUrl + '/datas');
+            this.response = data;
+        },
+        async createData() {
+            console.log(this.response.lenght);
+            const { data } = await Axios.post(apiUrl + '/data', {
+                info: this.info || 'default',
+                datas: this.response
+            });
             this.response = data;
         }
     },
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
     res.json({ test: 'hello world' });
 });
 
+router.get('/datas', function(req, res, next) {
+    let datas = [{ id: 1, info: {} }];
+    res.json({ datas });
+});
+
+router.post('/data', function(req, res, next) {
+    console.log('vody', req.body);
+    const { datas, info } = req.body;
+    datas.push(info);
+    res.json(datas);
+});
+
 module.exports = router;
